baffa5a7e6ad2847c4e4c8f2f7eb182897b0f7f0
Ilya Aizenberg <ilya86berg@gmail.com>
2024-02-08 19:38:08 +0200

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  css: ['~/assets/main.scss'],
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss"]
+})
